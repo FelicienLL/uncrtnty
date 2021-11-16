94ad3dc2a47b1f17eadc404c2ff8545269a15cd5
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2021-11-16 10:53:53 +0100

    omega2/se(omega2)

--- R/degree.R
+++ R/degree.R
@@ -4,6 +4,6 @@ compute_df <- function(est, se, add_1 = FALSE){
     est <- diag(est)
     se <- diag(se)
   }
-  df <- 2 * (sqrt(est) / se)^2 + as.integer(add_1)
-  round(max(df), 0)
+  df <- 2 * (est / se)^2 + as.integer(add_1)
+  df <- max(round(min(df), 0), length(df))
 }
--- tests/testthat/test-degree.R
+++ tests/testthat/test-degree.R
@@ -1,8 +1,8 @@
 test_that("degree works", {
-  expect_equal(compute_df(est = 0.2, se = 0.05), 160)
-  expect_equal(compute_df(est = matrix(0.2), se = matrix(0.05)), 160)
+  expect_equal(compute_df(est = 0.2, se = 0.05), 32)
+  expect_equal(compute_df(est = matrix(0.2), se = matrix(0.05)), 32)
   expect_error(compute_df(est = matrix(0.2), se = 0.05))
   expect_error(compute_df(est = matrix(c(0.2, 0, 0, 0.5), ncol = 2), se = matrix(0.05)))
-  expect_equal(compute_df(est = matrix(c(0.2, 0, 0, 0.5), ncol = 2), se =matrix(c(0.05, 0, 0, 0.1), ncol = 2)), 160)
-  expect_equal(compute_df(est = 0.5, se = 0.1), 100)
+  expect_equal(compute_df(est = matrix(c(0.2, 0, 0, 0.5), ncol = 2), se =matrix(c(0.05, 0, 0, 0.1), ncol = 2)), 32)
+  expect_equal(compute_df(est = 0.5, se = 0.1), 50)
 })
