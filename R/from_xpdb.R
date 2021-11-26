1c14e318de7f0ce03547ab3609740cacfebdfd64
FelicienLL <lelouedec.felicien@iuct-oncopole.fr>
2021-11-26 15:55:13 +0100

    rearrange R files

--- R/from_xpdb.R
+++ R/from_xpdb.R
@@ -30,3 +30,65 @@ u_from_xpdb <- function(xpdb){
   class(ans) <- "uncrtnty"
   return(ans)
 }
+
+# =============  UTILS to work on XPOSE objects =============
+
+#' Extract .lst file from xpose object
+#'
+#' @param xpdb an object of "xpose_data" class
+#'
+#' @return lst file in the form of a character vector.
+#' @export
+#'
+#' @examples
+#' x <- readRDS(system.file("xposerun", "xpdb_ex_pk.rds", package = "uncrtnty"))
+#' get_lst(x)
+get_lst <- function(xpdb){
+  stopifnot(inherits(xpdb, "xpose_data"))
+  xpdb$code$code[xpdb$code$subroutine=="lst"]
+}
+
+#' Extract .cov file from xpose object
+#'
+#' @param xpdb an object of "xpose_data" class
+#'
+#' @return cov file in the form of a tibble dataset.
+#' @export
+#'
+#' @examples
+#' x <- readRDS(system.file("xposerun", "xpdb_ex_pk.rds", package = "uncrtnty"))
+#' get_cov(x)
+get_cov <- function(xpdb){
+  stopifnot(inherits(xpdb, "xpose_data"))
+  xpdb$files$data[xpdb$files$extension=="cov"][[1]]
+}
+
+#' Extract .ext file from xpose object
+#'
+#' @param xpdb an object of "xpose_data" class
+#'
+#' @return ext file in the form of a tibble dataset.
+#' @export
+#'
+#' @examples
+#' x <- readRDS(system.file("xposerun", "xpdb_ex_pk.rds", package = "uncrtnty"))
+#' get_ext(x)
+get_ext <- function(xpdb){
+  stopifnot(inherits(xpdb, "xpose_data"))
+  xpdb$files$data[xpdb$files$extension=="ext"][[1]]
+}
+
+#' Extract .phi file from xpose object
+#'
+#' @param xpdb an object of "xpose_data" class
+#'
+#' @return phi file in the form of a tibble dataset.
+#' @export
+#'
+#' @examples
+#' x <- readRDS(system.file("xposerun", "xpdb_ex_pk.rds", package = "uncrtnty"))
+#' get_phi(x)
+get_phi <- function(xpdb){
+  stopifnot(inherits(xpdb, "xpose_data"))
+  xpdb$files$data[xpdb$files$extension=="phi"][[1]]
+}
